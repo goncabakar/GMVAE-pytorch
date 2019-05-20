4b22c12e00091a507c0a391c83ed9c55184b6350
Gonca Bakar <goncabakar@Goncas-MacBook-Pro.local>
2019-05-20 06:24:33 +0300

    gmm

---
+++ gmm.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from sklearn.mixture import GaussianMixture
+import scipy
+
+data = np.load("traj.npy")
+#data = data[1:100000]
+
+dim,_ = data.shape
+
+n_bins = 100
+plt.figure()
+n, bins, patches = plt.hist(data, bins=n_bins, density=True, histtype='bar', facecolor='g')
+plt.title('Histogram')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+num_comp = 4
+gmm = GaussianMixture(n_components = num_comp)
+gmm.fit(data)
+means = gmm.means_
+covar = gmm.covariances_
+probs = gmm.predict_proba(data)
+
+for i in range(num_comp):
+    num = sum(probs[:,i])
+    mu = means[i]
+    sigma = np.sqrt(covar[i])
+    y = scipy.stats.norm.pdf(bins, mu, sigma)
+    l = plt.plot(bins, np.transpose(y)*num/dim, 'r--', linewidth=1)
+
+
+plt.savefig("histogram"+str(num_comp)+".pdf")
